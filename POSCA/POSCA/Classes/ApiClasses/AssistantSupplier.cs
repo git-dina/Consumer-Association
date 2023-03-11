1a8771815a2d647b0d29e4a15a295871bd9605e5
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-03-12 01:46:33 +0200

    assistant supplier class

---
+++ POSCA/POSCA/Classes/ApiClasses/AssistantSupplier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes.ApiClasses
+{
+    public class AssistantSupplier
+    {
+        public long AssistantSupId { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool IsBlocked { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+    }
+}
