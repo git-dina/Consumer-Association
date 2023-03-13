ed6474b7fcb447870e555ea4e51dabd588a0a9ab
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2023-03-14 00:31:12 +0300

    wd_supplierAllowedOperations

---
+++ POSCA/POSCA/View/windows/wd_supplierAllowedOperations.xaml.cs
@@ -0,0 +1,145 @@
+﻿using POSCA.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POSCA.View.windows
+{
+    /// <summary>
+    /// Interaction logic for wd_supplierAllowedOperations.xaml
+    /// </summary>
+    public partial class wd_supplierAllowedOperations : Window
+    {
+
+        public wd_supplierAllowedOperations()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            { HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+        BrushConverter bc = new BrushConverter();
+
+
+
+        private void Btn_colse_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {//load
+
+            try
+            {
+
+
+                HelpClass.StartAwait(grid_main);
+
+                #region translate
+
+                //if (AppSettings.lang.Equals("en"))
+                //{
+                //    grid_main.FlowDirection = FlowDirection.LeftToRight;
+                //}
+                //else
+                //{
+                    grid_main.FlowDirection = FlowDirection.RightToLeft;
+                //}
+
+                translate();
+                #endregion
+              
+
+
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+
+        }
+
+
+        private void translate()
+        {
+            //
+
+            //txt_title.Text = AppSettings.resourcemanager.GetString("");
+
+
+            btn_save.Content = AppSettings.resourcemanager.GetString("trSave");
+
+        }
+
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Return)
+                {
+                    //Btn_save_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            { HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                e.Cancel = true;
+                this.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch { }
+        }
+
+        private async void Btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                HelpClass.StartAwait(grid_main);
+              
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+    }
+}
