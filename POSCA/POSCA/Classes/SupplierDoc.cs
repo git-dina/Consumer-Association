fa5d822711fba95dc71edf2dbdc3d72b33ec5474
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-03-15 01:05:12 +0200

    new classes

---
+++ POSCA/POSCA/Classes/SupplierDoc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes
+{
+    public class SupplierDoc
+    {
+        #region Attributes
+        public long DocumentId { get; set; }
+        public Nullable<long> SupId { get; set; }
+        public Nullable<long> TypeId { get; set; }
+        public string DocName { get; set; }
+        public string DocTitle { get; set; }
+        public Nullable<System.DateTime> StartDate { get; set; }
+        public Nullable<System.DateTime> EndDate { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        #endregion
+    }
+}
---
+++ POSCA/POSCA/Classes/SupplierDocType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes
+{
+    public class SupplierDocType
+    {
+        #region Attributes
+        public long TypeId { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        #endregion
+    }
+}
