48bb66f9b36b96fd3365ebe550994b0683337e3d
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2023-03-15 23:51:37 +0300

    uc_supplierDocType

---
+++ POSCA/POSCA/View/sectionData/vendors/uc_supplierDocType.xaml.cs
@@ -0,0 +1,453 @@
+﻿using netoaster;
+using POSCA.Classes;
+using POSCA.View.windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POSCA.View.sectionData.vendors
+{
+    /// <summary>
+    /// Interaction logic for uc_supplierDocType.xaml
+    /// </summary>
+    public partial class uc_supplierDocType : UserControl
+    {
+
+        public uc_supplierDocType()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private static uc_supplierDocType _instance;
+        public static uc_supplierDocType Instance
+        {
+            get
+            {
+                if (_instance is null)
+                    _instance = new uc_supplierDocType();
+                return _instance;
+            }
+            set
+            {
+                _instance = value;
+            }
+        }
+
+        SupplierDocType supplierDocType = new SupplierDocType();
+        IEnumerable<SupplierDocType> supplierDocTypesQuery;
+        string searchText = "";
+        public static List<string> requiredControlList;
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Instance = null;
+            GC.Collect();
+        }
+        private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {//load
+            try
+            {
+                HelpClass.StartAwait(grid_main);
+                requiredControlList = new List<string> { "Name" };
+                //if (AppSettings.lang.Equals("en"))
+                //{
+                //    grid_main.FlowDirection = FlowDirection.LeftToRight;
+                //}
+                //else
+                //{
+                grid_main.FlowDirection = FlowDirection.RightToLeft;
+                //}
+                translate();
+
+
+                //FillCombo.FillDefaultPayType_cashBalanceCardMultiple(cb_payType);
+                Keyboard.Focus(tb_Name);
+
+                await Search();
+                Clear();
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        private void translate()
+        {
+
+            //// Title
+            //if (!string.IsNullOrWhiteSpace(FillCombo.objectsList.Where(x => x.name == this.Tag.ToString()).FirstOrDefault().translate))
+            //    txt_title.Text = AppSettings.resourcemanager.GetString(
+            //   FillCombo.objectsList.Where(x => x.name == this.Tag.ToString()).FirstOrDefault().translate
+            //   );
+
+            txt_title.Text = AppSettings.resourcemanager.GetString("SupplierDocTypes");
+
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_search, AppSettings.resourcemanager.GetString("trSearchHint"));
+            txt_baseInformation.Text = AppSettings.resourcemanager.GetString("trBaseInformation");
+            //MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_code, AppSettings.resourcemanager.GetString("trCodeHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_Name, AppSettings.resourcemanager.GetString("trNameHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_Notes, AppSettings.resourcemanager.GetString("trNoteHint"));
+            txt_addButton.Text = AppSettings.resourcemanager.GetString("trAdd");
+            txt_updateButton.Text = AppSettings.resourcemanager.GetString("trUpdate");
+            txt_deleteButton.Text = AppSettings.resourcemanager.GetString("trDelete");
+
+            dg_supplierDocType.Columns[0].Header = AppSettings.resourcemanager.GetString("trName");
+            dg_supplierDocType.Columns[1].Header = AppSettings.resourcemanager.GetString("trNote");
+            btn_clear.ToolTip = AppSettings.resourcemanager.GetString("trClear");
+
+            tt_refresh.Content = AppSettings.resourcemanager.GetString("trRefresh");
+            btn_clear.ToolTip = AppSettings.resourcemanager.GetString("trClear");
+            //tt_report.Content = AppSettings.resourcemanager.GetString("trPdf");
+            //tt_print.Content = AppSettings.resourcemanager.GetString("trPrint");
+            //tt_excel.Content = AppSettings.resourcemanager.GetString("trExcel");
+            //tt_preview.Content = AppSettings.resourcemanager.GetString("trPreview");
+            //tt_pieChart.Content = AppSettings.resourcemanager.GetString("trPieChart");
+            tt_count.Content = AppSettings.resourcemanager.GetString("trCount");
+
+            //txt_active.Text = AppSettings.resourcemanager.GetString("trActive_");
+        }
+        #region Add - Update - Delete - Search - Tgl - Clear - DG_SelectionChanged - refresh
+        private async void Btn_add_Click(object sender, RoutedEventArgs e)
+        {//add
+            try
+            {
+                //if (FillCombo.groupObject.HasPermissionAction(basicsPermission, FillCombo.groupObjects, "add") || HelpClass.isAdminPermision())
+                {
+                    HelpClass.StartAwait(grid_main);
+
+                    supplierDocType = new SupplierDocType();
+                    if (HelpClass.validate(requiredControlList, this) && HelpClass.IsValidEmail(this))
+                    {
+
+                        supplierDocType.Name = tb_Name.Text;
+                        supplierDocType.Notes = tb_Notes.Text;
+                        supplierDocType.CreateUserId = MainWindow.userLogin.userId;
+
+                        FillCombo.supplierDocTypeList = await supplierDocType.save(supplierDocType);
+                        if (FillCombo.supplierDocTypeList == null)
+                            Toaster.ShowWarning(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trPopError"), animation: ToasterAnimation.FadeIn);
+                        else
+                        {
+                            Toaster.ShowSuccess(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trPopAdd"), animation: ToasterAnimation.FadeIn);
+
+                            Clear();
+                            await Search();
+                        }
+                    }
+                    HelpClass.EndAwait(grid_main);
+                }
+                //else
+                //    Toaster.ShowInfo(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trdontHavePermission"), animation: ToasterAnimation.FadeIn);
+
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private async void Btn_update_Click(object sender, RoutedEventArgs e)
+        {//update
+            try
+            {
+                //if (FillCombo.groupObject.HasPermissionAction(basicsPermission, FillCombo.groupObjects, "update") || HelpClass.isAdminPermision())
+                {
+                    HelpClass.StartAwait(grid_main);
+                    if (supplierDocType.TypeId > 0)
+                    {
+                        if (HelpClass.validate(requiredControlList, this) && HelpClass.IsValidEmail(this))
+                        {
+                            supplierDocType.Name = tb_Name.Text;
+                            supplierDocType.Notes = tb_Notes.Text;
+                            supplierDocType.UpdateUserId = MainWindow.userLogin.userId;
+
+                            FillCombo.supplierDocTypeList = await supplierDocType.save(supplierDocType);
+                            if (FillCombo.supplierDocTypeList == null)
+                                Toaster.ShowWarning(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trPopError"), animation: ToasterAnimation.FadeIn);
+                            else
+                            {
+                                Toaster.ShowSuccess(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trPopUpdate"), animation: ToasterAnimation.FadeIn);
+                                await Search();
+
+                            }
+                        }
+                    }
+                    else
+                        Toaster.ShowWarning(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trSelectItemFirst"), animation: ToasterAnimation.FadeIn);
+
+                    HelpClass.EndAwait(grid_main);
+                }
+                //else
+                //    Toaster.ShowInfo(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trdontHavePermission"), animation: ToasterAnimation.FadeIn);
+
+            }
+            catch (Exception ex)
+            {
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private async void Btn_delete_Click(object sender, RoutedEventArgs e)
+        {//delete
+            try
+            {
+                // if (FillCombo.groupObject.HasPermissionAction(basicsPermission, FillCombo.groupObjects, "delete") || HelpClass.isAdminPermision())
+                {
+                    HelpClass.StartAwait(grid_main);
+                    if (supplierDocType.TypeId != 0)
+                    {
+                        #region
+                        Window.GetWindow(this).Opacity = 0.2;
+                        wd_acceptCancelPopup w = new wd_acceptCancelPopup();
+                        w.contentText = AppSettings.resourcemanager.GetString("trMessageBoxDelete");
+
+                        w.ShowDialog();
+                        Window.GetWindow(this).Opacity = 1;
+                        #endregion
+
+                        if (w.isOk)
+                        {
+                            FillCombo.supplierDocTypeList = await supplierDocType.delete(supplierDocType.TypeId, MainWindow.userLogin.userId);
+                            if (FillCombo.supplierDocTypeList == null)
+                                Toaster.ShowWarning(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trPopError"), animation: ToasterAnimation.FadeIn);
+                            else
+                            {
+                                supplierDocType.TypeId = 0;
+                                Toaster.ShowSuccess(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trPopDelete"), animation: ToasterAnimation.FadeIn);
+
+                                await Search();
+                                Clear();
+                            }
+                        }
+
+                    }
+                    HelpClass.EndAwait(grid_main);
+                }
+                //else
+                //    Toaster.ShowInfo(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trdontHavePermission"), animation: ToasterAnimation.FadeIn);
+
+            }
+            catch (Exception ex)
+            {
+                Window.GetWindow(this).Opacity = 1;
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        #endregion
+        #region events
+        private async void Tb_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                HelpClass.StartAwait(grid_main);
+                await Search();
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        private void Btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HelpClass.StartAwait(grid_main);
+                Clear();
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private async void Dg_supplierDocType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                HelpClass.StartAwait(grid_main);
+                //selection
+
+                if (dg_supplierDocType.SelectedIndex != -1)
+                {
+                    supplierDocType = dg_supplierDocType.SelectedItem as SupplierDocType;
+                    this.DataContext = supplierDocType;
+                }
+                HelpClass.clearValidate(requiredControlList, this);
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private async void Btn_refresh_Click(object sender, RoutedEventArgs e)
+        {//refresh
+            try
+            {
+
+                HelpClass.StartAwait(grid_main);
+
+                tb_search.Text = "";
+                searchText = "";
+                await RefreshTypesList();
+                await Search();
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        #endregion
+        #region Refresh & Search
+        async Task Search()
+        {
+            //search
+            if (FillCombo.supplierDocTypeList is null)
+                await RefreshTypesList();
+            searchText = tb_search.Text.ToLower();
+            supplierDocTypesQuery = FillCombo.supplierDocTypeList.Where(s =>
+            s.Name.ToLower().Contains(searchText)
+            ).ToList();
+            RefreshTypesView();
+        }
+        async Task<IEnumerable<SupplierDocType>> RefreshTypesList()
+        {
+            await FillCombo.RefreshSupplierDocTypes();
+
+            return FillCombo.supplierDocTypeList;
+        }
+        void RefreshTypesView()
+        {
+            dg_supplierDocType.ItemsSource = supplierDocTypesQuery;
+            txt_count.Text = supplierDocTypesQuery.Count().ToString();
+        }
+        #endregion
+        #region validate - clearValidate - textChange - lostFocus - . . . . 
+        void Clear()
+        {
+            this.DataContext = new SupplierDocType();
+            dg_supplierDocType.SelectedIndex = -1;
+
+            // last 
+            HelpClass.clearValidate(requiredControlList, this);
+        }
+        string input;
+        decimal _decimal = 0;
+        private void Number_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+
+
+                //only  digits
+                TextBox textBox = sender as TextBox;
+                HelpClass.InputJustNumber(ref textBox);
+                if (textBox.Tag.ToString() == "int")
+                {
+                    Regex regex = new Regex("[^0-9]");
+                    e.Handled = regex.IsMatch(e.Text);
+                }
+                else if (textBox.Tag.ToString() == "decimal")
+                {
+                    input = e.Text;
+                    e.Handled = !decimal.TryParse(textBox.Text + input, out _decimal);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void Code_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+                //only english and digits
+                Regex regex = new Regex("^[a-zA-Z0-9. -_?]*$");
+                if (!regex.IsMatch(e.Text))
+                    e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+
+        }
+        private void Spaces_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                e.Handled = e.Key == Key.Space;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void ValidateEmpty_TextChange(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                HelpClass.validate(requiredControlList, this);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void validateEmpty_LostFocus(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HelpClass.validate(requiredControlList, this);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        #endregion
+
+
+
+
+    }
+}
