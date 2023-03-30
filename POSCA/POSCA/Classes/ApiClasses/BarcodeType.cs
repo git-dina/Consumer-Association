cef28633fa40eab7f7d6812810329f5b842615f8
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-03-31 01:50:10 +0300

    .

---
+++ POSCA/POSCA/Classes/ApiClasses/BarcodeType.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes.ApiClasses
+{
+    public class BarcodeType
+    {
+        #region Attributes
+        public int TypeId { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        #endregion
+
+        #region Methods
+        public async Task<List<BarcodeType>> save(BarcodeType type)
+        {
+            var result = new List<BarcodeType>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "BarcodeType/Save";
+
+            var myContent = JsonConvert.SerializeObject(type);
+            parameters.Add("itemObject", myContent);
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<BarcodeType>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<List<BarcodeType>> get(bool? isActive = null)
+        {
+            var result = new List<BarcodeType>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "BarcodeType/Get";
+
+            parameters.Add("isActive", isActive.ToString());
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<BarcodeType>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<List<BarcodeType>> delete(long typeId, long userId)
+        {
+            var result = new List<BarcodeType>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("itemId", typeId.ToString());
+            parameters.Add("userId", userId.ToString());
+            string method = "BarcodeType/delete";
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<BarcodeType>(c.Value));
+                }
+            }
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ POSCA/POSCA/Classes/ApiClasses/Brand.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes.ApiClasses
+{
+    public class Brand
+    {
+        #region Attributes
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool IsBlocked { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        #endregion
+
+        #region Methods
+        public async Task<List<Brand>> save(Brand brand)
+        {
+            var result = new List<Brand>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "Brand/Save";
+
+            var myContent = JsonConvert.SerializeObject(brand);
+            parameters.Add("itemObject", myContent);
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<Brand>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<List<Brand>> get(bool? isActive = null)
+        {
+            var result = new List<Brand>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "Brand/Get";
+
+            parameters.Add("isActive", isActive.ToString());
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<Brand>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<List<Brand>> delete(long brandId, long userId)
+        {
+            var result = new List<Brand>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("itemId", brandId.ToString());
+            parameters.Add("userId", userId.ToString());
+            string method = "Brand/delete";
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<Brand>(c.Value));
+                }
+            }
+            return result;
+        }
+        #endregion
+    }
+}
