18446f7ae6d267a02ec54ebab6bf464eb39c269d
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2023-04-03 00:16:22 +0300

    wd_itemGeneralization

---
+++ POSCA/POSCA/View/windows/wd_itemGeneralization.xaml.cs
@@ -0,0 +1,311 @@
+﻿using Microsoft.Win32;
+using POSCA.Classes;
+using POSCA.Classes.ApiClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace POSCA.View.windows
+{
+    /// <summary>
+    /// Interaction logic for wd_itemGeneralization.xaml
+    /// </summary>
+    public partial class wd_itemGeneralization : Window
+    {
+
+        public wd_itemGeneralization()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            { HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+        BrushConverter bc = new BrushConverter();
+
+
+
+        private void Btn_colse_Click(object sender, RoutedEventArgs e)
+        {
+            isOk = false;
+            this.Close();
+        }
+        List<ItemGeneralization> listItemGeneralization = new List<ItemGeneralization>();
+
+        public List<ItemGeneralization> ItemGeneralizations { get; set; }
+        public bool isOk { get; set; }
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {//load
+
+            try
+            {
+
+
+                HelpClass.StartAwait(grid_main);
+
+                #region translate
+
+                if (AppSettings.lang.Equals("en"))
+                {
+                    AppSettings.resourcemanager = new ResourceManager("POSCA.en_file", Assembly.GetExecutingAssembly());
+                    grid_main.FlowDirection = FlowDirection.LeftToRight;
+                }
+                else
+                {
+                    AppSettings.resourcemanager = new ResourceManager("POSCA.ar_file", Assembly.GetExecutingAssembly());
+                    grid_main.FlowDirection = FlowDirection.RightToLeft;
+                }
+
+                translate();
+                #endregion
+
+                setItemGeneralizationData();
+                
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+
+        }
+
+
+        private void translate()
+        {
+            //
+            //txt_title.Text = AppSettings.resourcemanager.GetString("MainDocuments");
+            //txt_itemGeneralization.Text = AppSettings.resourcemanager.GetString("ItemGeneralizations");
+
+            //dg_itemGeneralization.Columns[0].Header = AppSettings.resourcemanager.GetString("DocumentType");
+            //dg_itemGeneralization.Columns[1].Header = AppSettings.resourcemanager.GetString("trStartDate");
+
+            btn_save.Content = AppSettings.resourcemanager.GetString("trSave");
+
+        }
+
+       
+        private void setItemGeneralizationData()
+        {
+            dg_itemGeneralization.ItemsSource = ItemGeneralizations;
+        }
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Return)
+                {
+                    //Btn_save_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            { HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+        public static List<string> requiredControlList;
+        #region events
+
+        #endregion
+
+        #region validate - clearValidate - textChange - lostFocus - . . . . 
+        /*
+        string input;
+        decimal _decimal = 0;
+        private void Number_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+
+
+                //only  digits
+                TextBox textBox = sender as TextBox;
+                HelpClass.InputJustNumber(ref textBox);
+                if (textBox.Tag.ToString() == "int")
+                {
+                    Regex regex = new Regex("[^0-9]");
+                    e.Handled = regex.IsMatch(e.Text);
+                }
+                else if (textBox.Tag.ToString() == "decimal")
+                {
+                    input = e.Text;
+                    e.Handled = !decimal.TryParse(textBox.Text + input, out _decimal);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void Code_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+                //only english and digits
+                Regex regex = new Regex("^[a-zA-Z0-9. -_?]*$");
+                if (!regex.IsMatch(e.Text))
+                    e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+
+        }
+        private void Spaces_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                e.Handled = e.Key == Key.Space;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void ValidateEmpty_TextChange(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                HelpClass.validate(requiredControlList, this);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void validateEmpty_LostFocus(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HelpClass.validate(requiredControlList, this);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        */
+        #endregion
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                e.Cancel = true;
+                this.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch { }
+        }
+
+        private void Btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                // HelpClass.StartAwait(grid_main);
+                ItemGeneralizations = (List<ItemGeneralization>)dg_itemGeneralization.ItemsSource;
+
+                isOk = true;
+                this.Close();
+                // HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        private void Btn_addItemGeneralization_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                btn_addItemGeneralization.IsEnabled = false;
+                dg_itemGeneralization.IsEnabled = false;
+                listItemGeneralization.Add(new ItemGeneralization());
+                RefreshItemGeneralizationDataGrid();
+            }
+            catch (Exception ex)
+            {
+                btn_addItemGeneralization.IsEnabled = true;
+                dg_itemGeneralization.IsEnabled = true;
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        void deleteItemGeneralizationRowinDatagrid(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HelpClass.StartAwait(grid_main);
+
+                for (var vis = sender as Visual; vis != null; vis = VisualTreeHelper.GetParent(vis) as Visual)
+                    if (vis is DataGridRow)
+                    {
+
+                        btn_addItemGeneralization.IsEnabled = false;
+                        dg_itemGeneralization.IsEnabled = false;
+                        ItemGeneralization row = (ItemGeneralization)dg_itemGeneralization.SelectedItems[0];
+                        listItemGeneralization.Remove(row);
+                        RefreshItemGeneralizationDataGrid();
+                    }
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+                btn_addItemGeneralization.IsEnabled = true;
+                dg_itemGeneralization.IsEnabled = true;
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        public void RefreshItemGeneralizationDataGrid()
+        {
+            try
+            {
+                dg_itemGeneralization.CancelEdit();
+                dg_itemGeneralization.ItemsSource = listItemGeneralization;
+                dg_itemGeneralization.Items.Refresh();
+
+                dg_itemGeneralization.IsEnabled = true;
+                btn_addItemGeneralization.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                dg_itemGeneralization.IsEnabled = true;
+                btn_addItemGeneralization.IsEnabled = true;
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name, false);
+            }
+        }
+    }
+}
