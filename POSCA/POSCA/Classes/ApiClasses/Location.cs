1c35f3cfd4c54ffed9e8f81ebb783df3d752934f
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-05-02 16:50:55 +0300

    location

---
+++ POSCA/POSCA/Classes/ApiClasses/Location.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes.ApiClasses
+{
+    public class Location
+    {
+        public long LocationId { get; set; }
+        public Nullable<int> LocationTypeId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Notes { get; set; }
+        public bool IsBlocked { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+    }
+}
