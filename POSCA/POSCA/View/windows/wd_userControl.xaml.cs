bd36ee809a819c23bf0549e7f7842233514c1963
yasinidlbi <yasinidlbi@gmail.com>
2023-05-25 01:42:45 +0300

    add wd_userControl

---
+++ POSCA/POSCA/View/windows/wd_userControl.xaml.cs
@@ -0,0 +1,125 @@
+﻿using POSCA.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POSCA.View.windows
+{
+    /// <summary>
+    /// Interaction logic for wd_userControl.xaml
+    /// </summary>
+    public partial class wd_userControl : Window
+    {
+
+        public wd_userControl()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            { HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+
+        private void Btn_colse_Click(object sender, RoutedEventArgs e)
+        {
+            isOk = false;
+            this.Close();
+        }
+
+        public bool isOk { get; set; }
+        //public static List<string> requiredControlList;
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {//load
+
+            try
+            {
+
+
+                HelpClass.StartAwait(grid_main);
+                //requiredControlList = new List<string> { ""};
+
+                #region translate
+
+                if (AppSettings.lang.Equals("en"))
+                {
+                    grid_main.FlowDirection = FlowDirection.LeftToRight;
+                }
+                else
+                {
+                    grid_main.FlowDirection = FlowDirection.RightToLeft;
+                }
+
+                //translate();
+                #endregion
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+
+        }
+
+
+        private void translate()
+        {
+            
+        }
+
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Return)
+                {
+                    //Btn_save_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            { HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                e.Cancel = true;
+
+                this.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch { }
+        }
+      
+    }
+}
