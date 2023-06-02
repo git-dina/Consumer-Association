a09cc5a479bac3e112ee55b6a89822707d61bc58
yasinidlbi <yasinidlbi@gmail.com>
2023-06-03 02:29:29 +0300

    wd_receiptInv

---
+++ POSCA/POSCA/View/windows/wd_receiptInv.xaml.cs
@@ -0,0 +1,339 @@
+﻿using POSCA.Classes;
+using POSCA.Classes.ApiClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace POSCA.View.windows
+{
+    /// <summary>
+    /// Interaction logic for wd_receiptInv.xaml
+    /// </summary>
+    public partial class wd_receiptInv : Window
+    {
+
+        public wd_receiptInv()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            { HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+
+
+
+        private void Btn_colse_Click(object sender, RoutedEventArgs e)
+        {
+            isOk = false;
+            this.Close();
+        }
+
+        public bool isOk { get; set; }
+
+        public string invoiceType { get; set; }
+        public string invoiceStatus { get; set; }
+        public string windowTitle { get; set; }
+        public bool? isApproved { get; set; }
+
+        public Receipt receipt = new Receipt();
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {//load
+
+            try
+            {
+
+
+                HelpClass.StartAwait(grid_main);
+
+
+
+                #region translate
+
+                if (AppSettings.lang.Equals("en"))
+                {
+                    //AppSettings.resourcemanager = new ResourceManager("POSCA.en_file", Assembly.GetExecutingAssembly());
+                    grid_main.FlowDirection = FlowDirection.LeftToRight;
+                }
+                else
+                {
+                    //AppSettings.resourcemanager = new ResourceManager("POSCA.ar_file", Assembly.GetExecutingAssembly());
+                    grid_main.FlowDirection = FlowDirection.RightToLeft;
+                }
+
+                translate();
+                #endregion
+
+                await FillCombo.fillLocations(cb_LocationId);
+
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+
+        }
+
+
+        private void translate()
+        {
+            txt_title.Text = windowTitle;
+
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(cb_LocationId, AppSettings.resourcemanager.GetString("trBranchHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_searchInvNumber, AppSettings.resourcemanager.GetString("OrderNumberHint"));
+
+            dg_receipt.Columns[0].Header = AppSettings.resourcemanager.GetString("OrderNum");
+            dg_receipt.Columns[1].Header = AppSettings.resourcemanager.GetString("Location");
+            dg_receipt.Columns[2].Header = AppSettings.resourcemanager.GetString("DocumentDate");
+
+        }
+
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Return)
+                {
+                    //Btn_save_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            { HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+        public static List<string> requiredControlList;
+        #region events
+        private async void Btn_search_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (cb_LocationId.SelectedValue != null)
+                    await searchInvoices((long)cb_LocationId.SelectedValue, tb_searchInvNumber.Text, invoiceType, invoiceStatus, isApproved);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void tb_search_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Return)
+                {
+                    Btn_search_Click(btn_search, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private async Task searchInvoices(long locationId, string invNumber, string invoiceType, string invoiceStatus, bool? isApproved)
+        {
+            try
+            {
+                /*
+                HelpClass.StartAwait(grid_main);
+                var invoices = await receipt.searchOrders(locationId, invNumber, invoiceType, invoiceStatus, isApproved);
+                dg_receipt.ItemsSource = invoices;
+                dg_receipt.Items.Refresh();
+                HelpClass.EndAwait(grid_main);
+                */
+            }
+            catch
+            {
+                HelpClass.EndAwait(grid_main);
+            }
+        }
+
+
+        private void Btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                if (dg_receipt.SelectedIndex > -1)
+                {
+                    receipt = dg_receipt.SelectedItem as Receipt;
+                    isOk = true;
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        #endregion
+
+        #region validate - clearValidate - textChange - lostFocus - . . . . 
+
+        string input;
+        decimal _decimal = 0;
+        private void Number_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+
+
+                //only  digits
+                TextBox textBox = sender as TextBox;
+                HelpClass.InputJustNumber(ref textBox);
+                if (textBox.Tag.ToString() == "int")
+                {
+                    Regex regex = new Regex("[^0-9]");
+                    e.Handled = regex.IsMatch(e.Text);
+                }
+                else if (textBox.Tag.ToString() == "decimal")
+                {
+                    input = e.Text;
+                    e.Handled = !decimal.TryParse(textBox.Text + input, out _decimal);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void Code_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+                //only english and digits
+                Regex regex = new Regex("^[a-zA-Z0-9. -_?]*$");
+                if (!regex.IsMatch(e.Text))
+                    e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+
+        }
+        private void Spaces_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                e.Handled = e.Key == Key.Space;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void ValidateEmpty_TextChange(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                HelpClass.validate(requiredControlList, this);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void validateEmpty_LostFocus(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HelpClass.validate(requiredControlList, this);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        #endregion
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                //if (timer != null)
+                //    timer.Stop();
+                e.Cancel = true;
+                this.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch { }
+        }
+
+        private void cb_LocationId_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            try
+            {
+                var tb = cb_LocationId.Template.FindName("PART_EditableTextBox", cb_LocationId) as TextBox;
+                tb.FontFamily = Application.Current.Resources["Font-cairo-regular"] as FontFamily;
+                cb_LocationId.ItemsSource = FillCombo.locationsList.Where(p => p.Name.ToLower().Contains(tb.Text.ToLower()) || p.LocationId.ToString().Contains(tb.Text)).ToList();
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        private void dg_receipt_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                receipt = dg_receipt.SelectedItem as Receipt;
+                isOk = true;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        private void Dg_receipt_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                //receipt = dg_receipt.SelectedItem as Receipt;
+                //isOk = true;
+                //this.Close();
+            }
+            catch (Exception ex)
+            {
+                // HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+    }
+}
