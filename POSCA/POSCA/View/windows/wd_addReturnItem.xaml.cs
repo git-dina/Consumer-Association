98b3a4bfc92fed6741915b3a6a58a98e23fa1db7
yasinidlbi <yasinidlbi@gmail.com>
2023-06-07 15:46:51 +0300

    add wd_addReturnItem

---
+++ POSCA/POSCA/View/windows/wd_addReturnItem.xaml.cs
@@ -0,0 +1,274 @@
+﻿using netoaster;
+using POSCA.Classes;
+using POSCA.Classes.ApiClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace POSCA.View.windows
+{
+    /// <summary>
+    /// Interaction logic for wd_addReturnItem.xaml
+    /// </summary>
+    public partial class wd_addReturnItem : Window
+    {
+
+        public wd_addReturnItem()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            { HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+
+
+        private void Btn_colse_Click(object sender, RoutedEventArgs e)
+        {
+            isOk = false;
+            this.Close();
+        }
+        public PurchaseInvDetails newPurchaseItem = new PurchaseInvDetails();
+        public RecieptDetails newReceiptItem = new RecieptDetails();
+        public bool isOk { get; set; }
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {//load
+
+            try
+            {
+                HelpClass.StartAwait(grid_main);
+                //requiredControlList = new List<string> { "Name" };
+                requiredControlList = new List<string>();
+
+                #region translate
+
+                if (AppSettings.lang.Equals("en"))
+                {
+                    //AppSettings.resourcemanager = new ResourceManager("POSCA.en_file", Assembly.GetExecutingAssembly());
+                    grid_main.FlowDirection = FlowDirection.LeftToRight;
+                }
+                else
+                {
+                    //AppSettings.resourcemanager = new ResourceManager("POSCA.ar_file", Assembly.GetExecutingAssembly());
+                    grid_main.FlowDirection = FlowDirection.RightToLeft;
+                }
+
+                translate();
+                #endregion
+
+                if (newPurchaseItem.ItemId != null)
+                    this.DataContext = newPurchaseItem;
+                else
+                    this.DataContext = newReceiptItem;
+                HelpClass.EndAwait(grid_main);
+
+                tb_MinQty.Focus();
+
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+
+        }
+
+
+        private void translate()
+        {
+            //
+
+
+            txt_title.Text = AppSettings.resourcemanager.GetString("Item");
+            txt_itemDetails.Text = AppSettings.resourcemanager.GetString("ItemDetails");
+
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_Barcode, AppSettings.resourcemanager.GetString("trBarcodeHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_ItemCode, AppSettings.resourcemanager.GetString("ItemNumberHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_ItemName, AppSettings.resourcemanager.GetString("itemNameHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_ItemUnit, AppSettings.resourcemanager.GetString("trUnit"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_Factor, AppSettings.resourcemanager.GetString("FactorHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_Balance, AppSettings.resourcemanager.GetString("trBalanceHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_MinQty, AppSettings.resourcemanager.GetString("PiecesQuantityHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_Cost, AppSettings.resourcemanager.GetString("CostHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_CoopDiscount, AppSettings.resourcemanager.GetString("EnterpriseDiscountHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_ConsumerDiscount, AppSettings.resourcemanager.GetString("ConsumerDiscountHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_Price, AppSettings.resourcemanager.GetString("trPriceHint"));
+
+
+            btn_save.Content = AppSettings.resourcemanager.GetString("trAdd");
+        }
+
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Return)
+                {
+                    //Btn_save_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            { HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+        public static List<string> requiredControlList;
+        #region events
+
+        #endregion
+
+        #region validate - clearValidate - textChange - lostFocus - . . . . 
+        void Clear()
+        {
+            this.DataContext = new Supplier();
+            //txt_deleteButton.Text = AppSettings.resourcemanager.GetString("trDelete");
+
+            // last 
+            HelpClass.clearValidate(requiredControlList, this);
+        }
+        string input;
+        decimal _decimal = 0;
+        private void Number_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+
+
+                //only  digits
+                TextBox textBox = sender as TextBox;
+                HelpClass.InputJustNumber(ref textBox);
+                if (textBox.Tag.ToString() == "int")
+                {
+                    Regex regex = new Regex("[^0-9]");
+                    e.Handled = regex.IsMatch(e.Text);
+                }
+                else if (textBox.Tag.ToString() == "decimal")
+                {
+                    input = e.Text;
+                    e.Handled = !decimal.TryParse(textBox.Text + input, out _decimal);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void Code_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+                //only english and digits
+                Regex regex = new Regex("^[a-zA-Z0-9. -_?]*$");
+                if (!regex.IsMatch(e.Text))
+                    e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+
+        }
+        private void Spaces_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                e.Handled = e.Key == Key.Space;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void ValidateEmpty_TextChange(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                HelpClass.validate(requiredControlList, this);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void validateEmpty_LostFocus(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HelpClass.validate(requiredControlList, this);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        #endregion
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                //if (timer != null)
+                //    timer.Stop();
+                e.Cancel = true;
+                this.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch { }
+        }
+
+        private void Btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                // HelpClass.StartAwait(grid_main);
+                if (HelpClass.validate(requiredControlList, this) && HelpClass.IsValidEmail(this))
+                {
+                    if ((tb_MinQty.Text == "" || tb_MinQty.Text.Equals("0")))
+                        Toaster.ShowWarning(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("EnterMaxOrMinError"), animation: ToasterAnimation.FadeIn);
+                    else
+                    {
+                        isOk = true;
+                        this.Close();
+                    }
+                }
+                // HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+
+
+    }
+}
