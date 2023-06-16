aefa5c971c38f5a72fa53b92c8e7ac4c4136bdbc
yasinidlbi <yasinidlbi@gmail.com>
2023-06-17 01:58:36 +0300

    converters

---
+++ POSCA/POSCA/converters/PromotionNatureConverter.cs
@@ -0,0 +1,40 @@
+﻿using POSCA.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace POSCA.converters
+{
+     public class PromotionNatureConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string s = value as string;
+                switch (value)
+                {
+                    case "continual":
+                        return AppSettings.resourcemanager.GetString("ContinualOffer");
+                    case "temporary":
+                        return AppSettings.resourcemanager.GetString("TemporaryOffer");
+                    default:
+                        return "";
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ POSCA/POSCA/converters/PromotionTypeConverter.cs
@@ -0,0 +1,41 @@
+﻿using POSCA.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace POSCA.converters
+{
+     public class PromotionTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+
+                string s = value as string;
+                switch (value)
+                {
+                    case "quantity":
+                        return AppSettings.resourcemanager.GetString("QuantityOffer");
+                    case "percentage":
+                        return AppSettings.resourcemanager.GetString("PercentageOffer");
+                    default:
+                        return "";
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
