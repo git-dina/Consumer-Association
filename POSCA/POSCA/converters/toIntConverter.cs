ac43951b5282b066bfefa5e5d233a98909b95f5e
yasinidlbi <yasinidlbi@gmail.com>
2023-06-25 00:56:06 +0300

    Create toIntConverter.cs

---
+++ POSCA/POSCA/converters/toIntConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace POSCA.converters
+{
+     public class toIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value != null)
+                {
+                    int num = int.Parse(value.ToString());
+                    return num;
+                }
+                else return 0;
+
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                throw new NotImplementedException();
+
+            }
+            catch
+            {
+                return value;
+            }
+        }
+
+
+    }
+}
