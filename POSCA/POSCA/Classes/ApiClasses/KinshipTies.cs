74c67beb334cb359325ac94ae34ff407398ec32c
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-07-05 13:50:38 +0300

    add KinshipTies

---
+++ POSCA/POSCA/Classes/ApiClasses/KinshipTies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes.ApiClasses
+{
+    public class KinshipTies
+    {
+        #region Attributes
+        public int KinshipId { get; set; }
+        public string Name { get; set; }
+        public bool IsBlocked { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        #endregion
+    }
+}
