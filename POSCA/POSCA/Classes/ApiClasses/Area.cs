fbfe4890ad803b64a290fcff44a2dc88cfbd1132
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-07-05 16:00:58 +0300

    Update Area.cs

--- POSCA/POSCA/Classes/ApiClasses/Area.cs
+++ POSCA/POSCA/Classes/ApiClasses/Area.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +22,67 @@ namespace POSCA.Classes.ApiClasses
 
         //extra
         public List<Section> Sections { get; set; }
+        #endregion
+
+        #region Methods
+        public async Task<List<Area>> save(Area area)
+        {
+            var result = new List<Area>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "Area/Save";
+
+            var myContent = JsonConvert.SerializeObject(area);
+            parameters.Add("itemObject", myContent);
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<Area>(c.Value));
+                }
+            }
+            return result;
+        }
+
+
+        public async Task<List<Area>> get(bool? isActive = null)
+        {
+            var result = new List<Area>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "Area/Get";
+
+            parameters.Add("isActive", isActive.ToString());
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<Area>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<List<Area>> delete(int areaId, long userId)
+        {
+            var result = new List<Area>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("itemId", areaId.ToString());
+            parameters.Add("userId", userId.ToString());
+            string method = "Area/delete";
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<Area>(c.Value));
+                }
+            }
+            return result;
+        }
         #endregion
     }
 }
