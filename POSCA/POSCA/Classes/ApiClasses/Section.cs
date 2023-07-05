db32a351d7c3ba9924ac4f96ce2ca7441b21e842
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-07-05 13:27:10 +0300

    Area - Section classes

---
+++ POSCA/POSCA/Classes/ApiClasses/Section.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes.ApiClasses
+{
+    public class Section
+    {
+        #region Attributes
+        public int SectionId { get; set; }
+        public Nullable<int> AreaId { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        #endregion
+    }
+}
