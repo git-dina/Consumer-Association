55bad3547c218bed698781d7b1997baed633238e
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-07-15 03:02:36 +0300

    .

--- POSCA/POSCA/Classes/ApiClasses/CustomerBank.cs
+++ POSCA/POSCA/Classes/ApiClasses/CustomerBank.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +23,83 @@ namespace POSCA.Classes.ApiClasses
         public Nullable<System.DateTime> UpdateDate { get; set; }
         public Nullable<long> CreateUserId { get; set; }
         public Nullable<long> UpdateUserId { get; set; }
+        #endregion
+
+        #region Methods
+        public async Task<List<CustomerBank>> save(CustomerBank CustomerBank)
+        {
+            var result = new List<CustomerBank>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "CustomerBank/Save";
+
+            var myContent = JsonConvert.SerializeObject(CustomerBank);
+            parameters.Add("itemObject", myContent);
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<CustomerBank>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<List<CustomerBank>> get(bool? isActive = null)
+        {
+            var result = new List<CustomerBank>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "CustomerBank/Get";
+
+            parameters.Add("isActive", isActive.ToString());
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<CustomerBank>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<List<CustomerBank>> delete(long CustomerBankId, long userId)
+        {
+            var result = new List<CustomerBank>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("itemId", CustomerBankId.ToString());
+            parameters.Add("userId", userId.ToString());
+            string method = "CustomerBank/delete";
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<CustomerBank>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<String> getMaxBankId()
+        {
+            var result = "";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "CustomerBank/getMaxBankId";
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result = c.Value;
+                }
+            }
+            return result;
+        }
         #endregion
     }
 }
--- POSCA/POSCA/Classes/ApiClasses/HirarachyStructure.cs
+++ POSCA/POSCA/Classes/ApiClasses/HirarachyStructure.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +20,83 @@ namespace POSCA.Classes.ApiClasses
         public Nullable<System.DateTime> UpdateDate { get; set; }
         public Nullable<long> CreateUserId { get; set; }
         public Nullable<long> UpdateUserId { get; set; }
+        #endregion
+
+        #region Methods
+        public async Task<List<HirarachyStructure>> save(HirarachyStructure HirarachyStructure)
+        {
+            var result = new List<HirarachyStructure>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "HirarachyStructure/Save";
+
+            var myContent = JsonConvert.SerializeObject(HirarachyStructure);
+            parameters.Add("itemObject", myContent);
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<HirarachyStructure>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<List<HirarachyStructure>> get(bool? isActive = null)
+        {
+            var result = new List<HirarachyStructure>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "HirarachyStructure/Get";
+
+            parameters.Add("isActive", isActive.ToString());
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<HirarachyStructure>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<List<HirarachyStructure>> delete(long HirarachyStructureId, long userId)
+        {
+            var result = new List<HirarachyStructure>();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("itemId", HirarachyStructureId.ToString());
+            parameters.Add("userId", userId.ToString());
+            string method = "HirarachyStructure/delete";
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result.Add(JsonConvert.DeserializeObject<HirarachyStructure>(c.Value));
+                }
+            }
+            return result;
+        }
+
+        public async Task<String> getMaxhirarachyId()
+        {
+            var result = "";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string method = "HirarachyStructure/getMaxhirarachyId";
+
+            IEnumerable<Claim> claims = await APIResult.getList(method, parameters);
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    result = c.Value;
+                }
+            }
+            return result;
+        }
         #endregion
     }
 }
