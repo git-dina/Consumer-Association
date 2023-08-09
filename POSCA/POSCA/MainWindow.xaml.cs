211741e175094ba50a6d65abb761f53e3d086621
yasinidlbi <yasinidlbi@gmail.com>
2023-08-09 16:34:44 +0300

    customerData + scroll main

--- POSCA/POSCA/MainWindow.xaml.cs
+++ POSCA/POSCA/MainWindow.xaml.cs
@@ -1017,6 +1017,21 @@ namespace POSCA
                 HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
             }
         }
+        private void Btn_customerData_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                grid_main.Children.Clear();
+                grid_main.Children.Add(uc_customerData.Instance);
+
+                Button button = sender as Button;
+                secondMenuTitleActivate(button.Tag.ToString());
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
         private void Btn_generalSettings_Click(object sender, RoutedEventArgs e)
         {
             try
