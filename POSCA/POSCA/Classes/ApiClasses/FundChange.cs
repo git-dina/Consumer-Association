17ca68dfb17b99adf302016b9d868c0f03966165
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-08-20 21:57:55 +0300

    FundChange.cs

---
+++ POSCA/POSCA/Classes/ApiClasses/FundChange.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes.ApiClasses
+{
+    public  class FundChange
+    {
+        #region Methods
+        public long Id { get; set; }
+        public Nullable<long> CustomerId { get; set; }
+        public Nullable<long> OldFundNumber { get; set; }
+        public Nullable<long> NewFundNumber { get; set; }
+        public Nullable<long> SecondCustomerId { get; set; }
+        public string ChangeType { get; set; }
+        public string Reason { get; set; }
+        public Nullable<System.DateTime> ChangeDate { get; set; }
+        public Nullable<long> EmptyFundNumber { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        #endregion
+    }
+}
