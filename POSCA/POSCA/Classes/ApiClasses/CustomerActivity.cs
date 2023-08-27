948c32106bb7935c6ec92f8e3cbc8bfa3f023284
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-08-28 00:06:02 +0300

    CustomerActivity class

---
+++ POSCA/POSCA/Classes/ApiClasses/CustomerActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes.ApiClasses
+{
+    public class CustomerActivity
+    {
+        #region Attributes
+        public long RequestId { get; set; }
+        public Nullable<long> CustomerId { get; set; }
+        public Nullable<long> BoxNumber { get; set; }
+        public Nullable<long> ActivityId { get; set; }
+        public int Count { get; set; }
+        public string Notes { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+
+        //extra
+        public string CustomerName { get; set; }
+        public string CustomerStatus { get; set; }
+        public string CivilNum { get; set; }
+        public bool FamilyCardHolder { get; set; }
+
+        //activity
+        public string ActivityName { get; set; }
+        public decimal BasicValue { get; set; }
+        public decimal ValueAfterDiscount { get; set; }
+        public int MaximumBenefit { get; set; }
+        public Nullable<System.DateTime> StartDate { get; set; }
+        public Nullable<System.DateTime> EndDate { get; set; }
+        public Nullable<System.DateTime> RegistrationDate { get; set; }
+
+        #endregion
+    }
+}
