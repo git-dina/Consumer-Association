05e8f0483c2a5d4d1251a1b4527874062602f2fb
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-08-29 01:21:59 +0300

    CustomerBankAccount

---
+++ POSCA/POSCA/Classes/ApiClasses/CustomerBankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSCA.Classes.ApiClasses
+{
+    public class CustomerBankAccount
+    {
+        public long BankAccountId { get; set; }
+        public Nullable<long> CustomerId { get; set; }
+        public string OldIBAN { get; set; }
+        public string NewIBAN { get; set; }
+        public Nullable<int> OldBankId { get; set; }
+        public Nullable<int> NewBankId { get; set; }
+        public string Notes { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        //extra
+        public string OldBankName { get; set; }
+        public string NewBankName { get; set; }
+    }
+}
