4a1d4e895b05f8b11ec4b38f06354836348c493d
yasinidlbi <yasinidlbi@gmail.com>
2023-09-09 02:32:46 +0300

    Update uc_phoneType.xaml.cs

--- POSCA/POSCA/View/sectionData/uc_phoneType.xaml.cs
+++ POSCA/POSCA/View/sectionData/uc_phoneType.xaml.cs
@@ -19,6 +19,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 namespace POSCA.View.sectionData
 {
     /// <summary>
