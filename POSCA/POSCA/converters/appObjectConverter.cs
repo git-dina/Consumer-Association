5b8472a07ba0ebb893a4b4c82e1b6d7565882bbd
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-09-09 15:03:54 +0300

    converter

---
+++ POSCA/POSCA/converters/appObjectConverter.cs
@@ -0,0 +1,44 @@
+﻿using POSCA.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace POSCA.converters
+{
+    internal class appObjectConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+
+                switch (AppSettings.lang)
+                {
+                    case "ar":
+                        return values[0];
+                    case "en":
+                        return values[1];
+                  
+                    default:
+                        return "";
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+
+            string[] values = null;
+            if (value != null)
+                return values = value.ToString().Split(' ');
+            return values;
+        }
+    }
+}
